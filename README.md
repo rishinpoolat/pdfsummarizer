34e89f8d6749bc89ad45bddc7d62bdf880d25a5a
rishinpoolat <rishinpoolat@gmail.com>
2024-08-20 18:59:32 +0100

    completed

--- README.md
+++ README.md
@@ -1,16 +1,19 @@
 # PDF Summarizer
 
 ## Description
-The system is built with the aim of summarising a PDF file uploaded by the user. This system is created using Streamlit, LangChain, and OpenAI frameworks.
+
+The system is built with the aim of summarising a PDF file uploaded by the user. This system is created using Streamlit, LangChain, and HugggingFace frameworks.
 
 ## Preview
+
 To see a preview of this system, check out the preview below.
 
-<img src="preview.jpg" width="100%" />
+<img src="preview.png" width="100%" />
 
 ## How to Use
-In the lines of the program code below, change ``YOUR_OPENAI_API_KEY`` to match your OPENAI API KEY. This line of code can be found in the ``.env.example`` file.
+
+In the lines of the program code below, change `YOUR_OPENAI_API_KEY` to match your HUGGINGFACEHUB_API_TOKEN. This line of code can be found in the `.env.example` file.
 
 ```bash
-OPENAI_API_KEY=YOUR_OPENAI_API_KEY
+HUGGINGFACEHUB_API_TOKEN=YOUR_OPENAI_API_KEY
 ```
--- app.py
+++ app.py
@@ -3,20 +3,22 @@ from dotenv import load_dotenv
 from PyPDF2 import PdfReader
 import streamlit as st
 from langchain.text_splitter import CharacterTextSplitter
-from langchain_community.embeddings.openai import OpenAIEmbeddings
+from langchain_community.embeddings import HuggingFaceEmbeddings
 from langchain_community.vectorstores import FAISS
 from langchain_community.chat_models import ChatOpenAI
-from langchain_community.llms import OpenAI
+from langchain_community.llms import HuggingFaceHub
+from huggingface_hub import login
 from langchain.chains.question_answering import load_qa_chain
 from langchain_community.callbacks import get_openai_callback
 
 
-def load_openai_api_key():
+def load_huggingface_api_token():
     load_dotenv()
-    openai_api_key = os.getenv("OPENAI_API_KEY")
-    if not openai_api_key:
+    huggingface_token = os.getenv("HUGGINGFACEHUB_API_TOKEN")
+    print(huggingface_token)
+    if not huggingface_token:
         raise ValueError("Unable to retrieve open ai api key")
-    return openai_api_key
+    return huggingface_token
 
 def text_processing_from_pdf(pdf):
     
@@ -28,8 +30,9 @@ def text_processing_from_pdf(pdf):
         
     text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=200, length_function=len)
     chunks = text_splitter.split_text(text)
-    embeddings = OpenAIEmbeddings()
+    embeddings = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
     knowledge_base = FAISS.from_texts(chunks, embeddings)
+    # ... existing code ...
     
     return knowledge_base
 
@@ -40,7 +43,8 @@ def main():
     st.divider()
 
     try:
-        os.environ["OPENAI_API_KEY"] = load_openai_api_key()
+        huggingface_token = load_huggingface_api_token()
+        login(token=huggingface_token)
     except ValueError as e:
         st.error(str(e))
         return  
@@ -54,13 +58,11 @@ def main():
 
         if query:
             docs = knowledge_base.similarity_search(query)
-            open_ai_model = "gpt-3.5-turbo-16k"
-            llm = ChatOpenAI(model=open_ai_model, temperature=0.1)
+            llm = HuggingFaceHub(repo_id="google/flan-t5-large", model_kwargs={"temperature": 0.5, "max_length": 512})
             chain = load_qa_chain(llm, chain_type='stuff')
 
-            with get_openai_callback() as cost:
-                response = chain.run(input_documents=docs, question=query)
-                print(cost)
+            response = chain.run(input_documents=docs, question=query)
+
 
             st.subheader('Summary Results:')
             st.write(response)
